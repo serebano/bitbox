be0f89e5910d06acdcc1612efd140234c4660100
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-02-18 11:40:42 +0200

    init2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Tag from 'cerebral/lib/tags/Tag'
 
 //import App from './components/App'
 Tag.create = (...args) => new Tag(...args)
+Tag.create2 = (...args) => new Tag(...args)
 
 export default Tag
 
