d2010c1914ece37b80306ca2b00830bc4f43e102
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-02-21 09:42:13 +0200

    added utils

--- src/index.js
+++ src/index.js
@@ -6,28 +6,134 @@ import compute from './handlers/compute'
 import observe from './handlers/observe'
 import * as operators from './operators'
 import debug from './debug'
+import * as utils from './utils'
+import DependencyStore from 'cerebral/lib/DependencyStore'
 
+const app = {
+	deps: new DependencyStore(),
+	changes: []
+}
+
+
+const flush = (force) => {
+	const changes = app.changes
+	app.changes = []
+	let components = []
+
+	if (force) {
+    	components = app.deps.getAllUniqueEntities()
+    } else {
+    	components = app.deps.getUniqueEntities(changes)
+    }
+
+	components.forEach((component) => {
+	  	component(changes)
+	})
+
+	return {changes,components}
+}
+
+const run = (...actions) => {
+	const result = actions.map(action => action(context))
+	const changes = flush()
+	return {
+		result,
+		...changes
+	}
+}
 const props = observe('props', (path, value) => {
-	console.log(`(props)`, path, '=', value)
+	app.changes.push({ type: 'props', path: path.split("."), forceChildPathUpdates: false })
+	//console.log(`(props)`, path, '=', value)
 })
 
 const state = observe('state', (path, value) => {
-    console.info(`(state)`, path, '=', value)
+	app.changes.push({ type: 'state', path: path.split("."), forceChildPathUpdates: false })
+    //console.info(`(state)`, path, '=', value)
 })
-
-Object.assign(window, Tag.templates, operators)
 
 const context = {
     state: {
         app: {
             name: 'Falcon'
-        }
+        },
+		user: {
+			firstName: 'Sergiu',
+			lastName: 'Toderascu'
+		}
     },
     props: {
         color: 'red',
         count: 1
     }
 }
+
+
+const getDeps = ($, o={}) => {
+	return $.values.reduce((obj, tag) => {
+		if (tag.type === "state") {
+			const path = tag.path(context)
+			const strictPath = utils.ensureStrictPath(path, tag.get(context))
+			obj[strictPath] = true
+		} else if (tag.type === "compute.object" || tag.type === "compute.array") {
+			return getDeps(tag, obj)
+		}
+		return obj
+	}, o)
+}
+function Component(dependencies, component) {
+	const deps = compute(dependencies)
+	console.log('deps', deps)
+	const depsMap = getDeps(deps)
+	function updater(changes) {
+		component(deps.get(context))
+	}
+
+	app.deps.addEntity(updater, depsMap)
+
+	return {
+		dependencies, component, deps, depsMap, updater
+	}
+}
+
+const fooComp = Component({
+		data: [
+			state`app`,
+			{
+				fullName: [
+					state`user.firstName`,
+					state`user.lastName`,
+					(f,l) => f + " " + l
+				]
+			},
+			(app,user) => {
+				return { ...app, user }
+			}
+		]
+	},
+	function Foo(props) {
+		console.log(`Foo`, props)
+	}
+)
+
+const countComp = Component({
+		count: state`app.count`,
+		color: state`app.color`
+	},
+	function Count(props) {
+		console.log(`Count`, props)
+	}
+)
+
+const nameComp = Component({
+		name: state`app.name`
+	},
+	function Name(props) {
+		console.log(`Name`, props)
+	}
+)
+
+Object.assign(window, Tag.templates, operators, utils, {getDeps,Component,run,fooComp,countComp,nameComp})
+
 
 const arr = compute.array([ 1, 2, (a, b) => a + b ])
 const obj = compute.object({ arr, app: state`app`, bar: [1,2,3] })
@@ -61,6 +167,9 @@ const setAction = operators.set(state`app.str2`, props`str2`)
 window.incAction = incAction
 window.setAction = setAction
 
+window.flush = flush
+
+window.app = app
 window.arr = arr
 window.obj = obj
 window.mix = mix
