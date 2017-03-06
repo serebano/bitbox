5000266ee19e2b5478ceef616956e0ce95a74195
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-03-06 17:12:48 +0200

    inc fix

--- src/operators/inc.js
+++ src/operators/inc.js
@@ -1,10 +1,10 @@
-export default (target, value = 1) => {
+export default (target, value) => {
 
     function inc(context) {
         const model = context.model(target)
 
         model.set(null, model.get(null, (state = 0) => {
-            return state + context.resolve.value(value)
+            return state + context.resolve.value(value || 1)
         }))
     }
 
