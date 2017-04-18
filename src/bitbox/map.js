4e598b27c99f117def8e9715038939f6e58961be
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-04-18 17:55:30 +0300

    fix(map)

--- src/bitbox/map.js
+++ src/bitbox/map.js
@@ -1,28 +1,26 @@
-import { is } from "../utils";
-import bitbox from ".";
+import { is } from "../utils"
+import bitbox from "."
 
 /**
  * bitbox.map
- * @param  {Object} mapping
  * @param  {Object} target
+ * @param  {Object} mapping
  * @return {Object}
  */
 
 function map(target, mapping) {
-    return new Proxy(target, {
-        get(target, key) {
-            return Reflect.has(mapping, key)
-                ? bitbox.get(target, Reflect.get(mapping, key))
-                : Reflect.get(target, key);
+    return new Proxy(mapping, {
+        get(mapping, key) {
+            return Reflect.has(mapping, key) ? bitbox.get(target, Reflect.get(mapping, key)) : Reflect.get(target, key)
         },
-        set(target, key, value) {
-            const resolved = is.box(value) ? value(target) : value;
+        set(mapping, key, value) {
+            const resolved = is.box(value) ? value(target) : value
 
             return Reflect.has(mapping, key)
                 ? bitbox.set(target, Reflect.get(mapping, key), resolved)
-                : Reflect.set(target, key, resolved);
+                : Reflect.set(target, key, resolved)
         }
-    });
+    })
 }
 
-export default map;
+export default map
