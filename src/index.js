14942a1937d56dae0ebd8a3b448f1a94fcdee949
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-04-25 18:18:48 +0300

    babel setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import bitbox from "./bitbox"
 import * as operators from "./operators"
 import { is } from "./utils"
-import * as example from "./examples"
+//import * as example from "./examples"
 
 export default bitbox
+export { operators, is }
 
-Object.assign(window, operators, {
-    is,
-    bitbox,
-    operators
-})
-
-Object.assign(window, example)
+// Object.assign(window, operators, {
+//     is,
+//     bitbox,
+//     operators
+// })
+//
+// Object.assign(window, example)
