7e8c641e90dbcd5d4f4436892c755459b2987ba2
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-05-20 20:54:29 +0300

    cleaning

--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
-import is from "./is"
-import box from "./box"
-import observe from "./observe"
-import observable from "./observable"
-import project from "./project"
-import run, { action } from "./run"
-import bitbox from "./bitbox"
-import factory from "./factory"
-import resolve from "./resolve"
-import * as operators from "./operators"
-//import * as api from "./api/box"
-
-Object.assign(bitbox, {
-    factory,
-    project,
-    resolve,
-    observable,
-    observe,
-    box,
-    run,
-    action,
-    is,
-    operators
-})
-
-Object.assign(window, operators, {
-    bitbox,
-    factory,
-    project,
-    resolve,
-    observable,
-    observe,
-    box,
-    run,
-    is,
-    action
-})
-
-export { is, box, observe, observable, project, run, action, factory, resolve, operators }
-
-export default bitbox
-/* ... */
+export { default as is } from "./is"
+export { default as box } from "./box"
+export { default as factory } from "./factory"
+export { default as resolve } from "./resolve"
+export { default as project } from "./project"
+export { default as observe } from "./observe"
+export { default as observable } from "./observable"
