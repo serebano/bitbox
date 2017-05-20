0331a8cd824acc6b5f702fab79d591172fdb366c
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-05-20 05:37:41 +0300

    box(example)

--- src/api/box.js
+++ src/api/box.js
@@ -1,47 +1,38 @@
-import bitbox, { map, inc, concat, print } from "../bitbox"
 import box from "../box"
+import project from "../project"
+import observe from "../observe"
+import observable from "../observable"
 
 export const obj = {
-    state: {
+    count: 0,
+    items: []
+}
+
+export function inc() {
+    return ++this.count
+}
+
+export function on(fn) {
+    return observe(fn, this)
+}
+
+export const pro = project(
+    obj,
+    observable({
         count: 0,
-        items: []
-    }
-}
+        status: "idle",
+        inc
+    })
+)
 
-export const abox = box.a(observable, obj => project(obj, { count: 0, inc }))(obj)
-
-export const map1 = ({ state }, { observable }) => {
-    return {
-        state: state(observable),
-        foo: state(target => {
-            return {
-                bar: target.name,
-                items: target.items || (target.items = [])
-            }
-        })(observable)
-    }
-}
-
-export const map2 = ({ state, foo }) => {
-    return {
-        count: state.count,
-        foo: foo.bar,
-        items: foo.items,
-        x: state.name
-    }
-}
-
-export const map3 = map(map2, map1)
-
-export const app = bitbox(map3)
-
-app.count(obj, app.count(inc))
-
-bitbox(
-    target => [
-        app.items(concat(1, 2, 3))(target),
-        app.items(concat, 1, 2, 3)(target),
-        app.items(target).concat(1, 2, 3)
-    ],
-    print
+export const one = box.one(
+    observable,
+    project({
+        count: 0,
+        inc,
+        on
+    })
 )(obj)
+
+one.on(box.count(print))
+one.inc()
