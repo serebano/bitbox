c6bdea4bd0ce032b263bfcf9312ab69b8b1f48c8
Serebano <sergiu.toderascu@gmail.com>
2017-06-04 12:41:26 +0300

    sun-12-4

--- src/resolve.js
+++ src/resolve.js
@@ -1,7 +1,6 @@
 import is from "./is"
-import curry2 from "./internal/curry2"
+import curry from "./curry"
 import isFunction from "./internal/isFunction"
-import { curry, functionLength, functionName } from "./fantasy-helpers"
 import * as api from "./operators"
 /**
  * resolve()
@@ -12,7 +11,7 @@ import * as api from "./operators"
  * @return {Any}
  */
 
-function resolve(path, target) {
+export default curry(function resolve(path, target) {
     return path.reduce((obj, key, index, path) => {
         if (is.undefined(obj)) return obj
         if (is.array(key)) key = resolve(key, target)
@@ -25,9 +24,4 @@ function resolve(path, target) {
         }
         return value
     }, target)
-}
-
-//if (is.object(key)) return resolve(path.slice(index), key)
-//if (index === path.length - 1) return func(object, key, index, path)
-
-export default curry2(resolve)
+})
