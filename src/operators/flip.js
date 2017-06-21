dfab59b8177c86a7b50455e20c38acc0510753a8
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2017-06-22 01:33:11 +0300

    api @dev

--- src/operators/flip.js
+++ src/operators/flip.js
@@ -1,12 +1,14 @@
-import curry from "../curry"
+import rearg from "./rearg"
 
-export default curry(function flip(fn) {
-    function f(a, b) {
-        const args = Array.prototype.slice.call(arguments, 0)
-        args[0] = b
-        args[1] = a
-        return fn.apply(this, args)
-    }
-    f.displayName = fn.displayName
-    return curry(f, fn.length)
-})
+export default rearg([1, 0])
+
+// curry(function flip(fn) {
+//     function f(a, b) {
+//         const args = Array.prototype.slice.call(arguments, 0)
+//         args[0] = b
+//         args[1] = a
+//         return fn.apply(this, args)
+//     }
+//     f.displayName = fn.displayName
+//     return curry(f, fn.length)
+// })
