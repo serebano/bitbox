1bfa007007326956ef33db32b4bb73165be71dbc
Serebano <sergiu.toderascu@gmail.com>
2017-08-08 14:57:40 +0300

    __

--- src/curry.js
+++ src/curry.js
@@ -1,5 +1,6 @@
 import arity from "./arity"
 import is from "./is"
+import { getArgNames } from "./utils"
 
 const merge = (dest, origin) => {
     const newArgs = dest.map(i => {
@@ -16,12 +17,17 @@ const merge = (dest, origin) => {
 const actualLength = arr => arr.reduce((len, curr) => (is.placeholder(curr) ? len : len + 1), 0)
 
 const containsPlaceholder = args => args.some(is.placeholder)
+function fnToString(fn) {
+    return fn.displayName || fn.name || fn.toString()
+}
 
 function curry(fn, ...initialArgs) {
     const len = is.integer(initialArgs[0])
         ? initialArgs[0]
         : containsPlaceholder(initialArgs) ? initialArgs.length : fn.length
     if (is.integer(initialArgs[0])) initialArgs = []
+    const argNames = fn.argNames || getArgNames(fn)
+    const name = fn.displayName || fn.name
 
     const _curry = (...args) => {
         const f = (...newArgs) => {
@@ -31,6 +37,13 @@ function curry(fn, ...initialArgs) {
         }
 
         const fx = arity(len - actualLength(args), f)
+
+        fx[Symbol.for("functional/curryable")] = true
+        fx.displayName = name
+        fx.argNames = argNames.slice(fx.length)
+        fx.args = args
+
+        fx.toString = () => fnToString(fn) + "(" + args.map(String).join(", ") + ")"
 
         return fx
     }
