5510fc2db87d9218c68110ee8ee6ad234d1ab7d1
Serebano <sergiu.toderascu@gmail.com>
2017-08-08 15:05:55 +0300

    update

--- src/partial.js
+++
@@ -1,9 +0,0 @@
-import arity from "./arity"
-
-// Apply left arbitrary number of arguments
-
-function partial(fn, ...args) {
-    return arity(Math.max(0, fn.length - args.length), (...rest) => fn(...args, ...rest))
-}
-
-export default partial
